0e98d9c1ecb6b989c421bceebe14f63a9ebf12b7
Alg31 <148805525+Alg31@users.noreply.github.com>
2023-12-20 19:17:56 -0700

    idle racer

---
+++ idleRacers/idle-racers.js
@@ -0,0 +1,11 @@
+const idleRaceTrack = document.getElementById("idle-race-track");
+const ctx = idleRaceTrack.getContext('2d');
+
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
+idleRaceTrack.width = windowWidth / 2;
+idleRaceTrack.height = windowHeight / 2;
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, windowWidth / 2, windowHeight / 2);
